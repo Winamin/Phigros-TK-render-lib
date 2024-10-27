eddd5202bf6e1365171e8133d20fecf652f9521a
Beks <yingxuw814@gmail.com>
2024-10-27 22:44:02 +0800

    Add files via upload

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -93,30 +93,21 @@ macro_rules! validate_events {
 
 fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Result<(AnimFloat, AnimFloat)> {
     validate_events!(pgr);
-    assert!(!pgr.is_empty(), "PgrSpeedEvent cannot be empty.");
     assert_eq!(pgr[0].start_time, 0.0);
-
     let mut kfs = Vec::new();
-    let mut pos = 0.0;
-
-    for it in &pgr[..pgr.len() - 1] {
+    let mut pos = 0.;
+    kfs.extend(pgr[..pgr.len().saturating_sub(1)].iter().map(|it| {
         let from_pos = pos;
         pos += (it.end_time - it.start_time) * r * it.value;
-        kfs.push(Keyframe::new(it.start_time * r, from_pos, 2));
-    }
-
+        Keyframe::new(it.start_time * r, from_pos, 2)
+    }));
     let last = pgr.last().unwrap();
     kfs.push(Keyframe::new(last.start_time * r, pos, 2));
     kfs.push(Keyframe::new(max_time, pos + (max_time - last.start_time * r) * last.value, 0));
-
     for kf in &mut kfs {
         kf.value /= HEIGHT_RATIO;
     }
-
-    Ok((
-        AnimFloat::new(pgr.iter().map(|it| Keyframe::new(it.start_time * r, it.value, 0)).collect()),
-        AnimFloat::new(kfs),
-    ))
+    Ok((AnimFloat::new(pgr.iter().map(|it| Keyframe::new(it.start_time * r, it.value, 0)).collect()), AnimFloat::new(kfs)))
 }
 
 fn parse_float_events(r: f32, mut pgr: Vec<PgrEvent>) -> Result<AnimFloat> {
@@ -177,7 +168,7 @@ fn parse_notes(r: f32, mut pgr: Vec<PgrNote>, speed: &mut AnimFloat, height: &mu
                     1 => NoteKind::Click,
                     2 => NoteKind::Drag,
                     3 => {
-                        Let start_height + (pgr.hold_time * pgr.speed * r / HEIGHT_RATIO)
+                        let end_time = (pgr.time + pgr.hold_time) * r;
                         height.set_time(end_time);
                         let end_height = height.now();
                         NoteKind::Hold { end_time, end_height }
@@ -242,7 +233,7 @@ pub fn parse_phigros(source: &str, extra: ChartExtra) -> Result<Chart> {
             line.notes_above
                 .iter()
                 .chain(line.notes_below.iter())
-                .map(|note| note.time.not_nan())
+                .map(|note| note.time.not_nan() * r)
                 .max()
                 .unwrap_or_default()
                 * (60. / line.bpm / 32.)
