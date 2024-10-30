16351e7beb3bdcb5a65d56c6f573d11363052562
Beks <yingxuw814@gmail.com>
2024-10-30 22:15:16 +0800

    Update pgr.rs

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -93,17 +93,16 @@ macro_rules! validate_events {
 
 fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Result<(AnimFloat, AnimFloat)> {
     validate_events!(pgr);
-    assert!(!pgr.is_empty(), "PgrSpeedEvent cannot be empty.");
     assert_eq!(pgr[0].start_time, 0.0);
 
     let mut kfs = Vec::new();
     let mut pos = 0.0;
 
-    for it in &pgr[..pgr.len() - 1] {
+    kfs.extend(pgr[..pgr.len().saturating_sub(1)].iter().map(|it| {
         let from_pos = pos;
         pos += (it.end_time - it.start_time) * r * it.value;
-        kfs.push(Keyframe::new(it.start_time * r, from_pos, 2));
-    }
+        Keyframe::new(it.start_time * r, from_pos, 2)
+    }));
 
     let last = pgr.last().unwrap();
     kfs.push(Keyframe::new(last.start_time * r, pos, 2));
@@ -114,8 +113,8 @@ fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Res
     }
 
     Ok((
-        AnimFloat::new(pgr.iter().map(|it| Keyframe::new(it.start_time * r, it.value, 0)).collect()),
-        AnimFloat::new(kfs),
+        AnimFloat::new(pgr.iter().map(|it| Keyframe::new(it.start_time * r, it.value, 2)).collect()),
+        AnimFloat::new(kfs)
     ))
 }
 
