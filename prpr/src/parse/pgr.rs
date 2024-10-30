f93e10278f5f607161e2727d9192b7b5adb2741e
Beks <yingxuw814@gmail.com>
2024-10-30 22:53:29 +0800

    Add files via upload

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -94,28 +94,20 @@ macro_rules! validate_events {
 fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Result<(AnimFloat, AnimFloat)> {
     validate_events!(pgr);
     assert_eq!(pgr[0].start_time, 0.0);
-
     let mut kfs = Vec::new();
-    let mut pos = 0.0;
-
+    let mut pos = 0.;
     kfs.extend(pgr[..pgr.len().saturating_sub(1)].iter().map(|it| {
         let from_pos = pos;
         pos += (it.end_time - it.start_time) * r * it.value;
         Keyframe::new(it.start_time * r, from_pos, 2)
     }));
-
     let last = pgr.last().unwrap();
     kfs.push(Keyframe::new(last.start_time * r, pos, 2));
     kfs.push(Keyframe::new(max_time, pos + (max_time - last.start_time * r) * last.value, 0));
-
     for kf in &mut kfs {
         kf.value /= HEIGHT_RATIO;
     }
-
-    Ok((
-        AnimFloat::new(pgr.iter().map(|it| Keyframe::new(it.start_time * r, it.value, 2)).collect()),
-        AnimFloat::new(kfs)
-    ))
+    Ok((AnimFloat::new(pgr.iter().map(|it| Keyframe::new(it.start_time * r, it.value, 0)).collect()), AnimFloat::new(kfs)))
 }
 
 fn parse_float_events(r: f32, mut pgr: Vec<PgrEvent>) -> Result<AnimFloat> {
@@ -176,10 +168,9 @@ fn parse_notes(r: f32, mut pgr: Vec<PgrNote>, speed: &mut AnimFloat, height: &mu
                     1 => NoteKind::Click,
                     2 => NoteKind::Drag,
                     3 => {
-                    let start_height = height.now();
-                    let end_time = (pgr.time + pgr.hold_time) * r;
+                        let end_time = (pgr.time + pgr.hold_time) * r;
                         height.set_time(end_time);
-                        let end_height = height.now();
+                        let end_height = start_height + (end_time - time) * height.now();
                         NoteKind::Hold { end_time, end_height }
                     }
                     4 => NoteKind::Flick,
@@ -195,7 +186,7 @@ fn parse_notes(r: f32, mut pgr: Vec<PgrNote>, speed: &mut AnimFloat, height: &mu
                 height: pgr.floor_position / HEIGHT_RATIO,
 
                 above,
-                multiple_hint: true,
+                multiple_hint: false,
                 fake: false,
                 judge: JudgeStatus::NotJudged,
             })
