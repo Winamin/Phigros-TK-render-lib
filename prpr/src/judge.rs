706f5596537ead99669956fb2ba1ab51029d2d10
Beks <yingxuw814@gmail.com>
2024-10-31 12:48:18 +0800

    Update judge.rs Special effects

--- prpr/src/judge.rs
+++ prpr/src/judge.rs
@@ -833,7 +833,7 @@ impl Judge {
             };
             let line = &chart.lines[line_id];
             res.with_model(line.now_transform(res, &chart.lines) * note_transform, |res| {
-                if !mathes!(note_kind, NoteKind::Hold{..}){                                       
+                if !macthes!(note_kind, NoteKind::Hold{..}){                                       
                     res.emit_at_origin(line.notes[id as usize].rotation(line), res.res_pack.info.fx_perfect())
             });
             if let Some(sfx) = match note_kind {
