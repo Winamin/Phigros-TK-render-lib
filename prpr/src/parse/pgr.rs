68d29217d0db23d5443ec34597537289f0fd692e
Beks <yingxuw814@gmail.com>
2024-11-01 20:48:58 +0800

    Update pgr.rs

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -92,19 +92,29 @@ macro_rules! validate_events {
     };
 }
 
-fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Result<(AnimFloat, AnimFloat)> {
+fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32, smooth_value: f32) -> Result<(AnimFloat, AnimFloat)> {
     validate_events!(pgr);
     assert_eq!(pgr[0].start_time, 0.0);
     let mut kfs = Vec::new();
     let mut pos = 0.;
+
     kfs.extend(pgr[..pgr.len().saturating_sub(1)].iter().map(|it| {
         let from_pos = pos;
         pos += (it.end_time - it.start_time) * r * it.value;
+        
         Keyframe::new(it.start_time * r, from_pos, 2)
     }));
+
     let last = pgr.last().unwrap();
     kfs.push(Keyframe::new(last.start_time * r, pos, 2));
     kfs.push(Keyframe::new(max_time, pos + (max_time - last.start_time * r) * last.value, 0));
+
+    // -_________________
+    for i in 0..kfs.len() - 1 {
+        let current = &mut kfs[i];
+        let next = &mut kfs[i + 1];
+        current.value = current.value * (1.0 - smooth_value) + next.value * smooth_value;
+    }
     for kf in &mut kfs {
         kf.value /= HEIGHT_RATIO;
     }
