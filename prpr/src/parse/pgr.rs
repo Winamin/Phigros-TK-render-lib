88be6ab28d960fbf1d901e3eaba0e59ebb30376a
Beks <yingxuw814@gmail.com>
2024-11-01 19:56:55 +0800

    Update pgr.rs

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -92,29 +92,25 @@ macro_rules! validate_events {
     };
 }
 
-fn parse_speed_events(mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Result<(AnimFloat, AnimFloat)> {
+fn parse_speed_events(r: f32, mut pgr: Vec<PgrSpeedEvent>, max_time: f32) -> Result<(AnimFloat, AnimFloat)> {
     validate_events!(pgr);
     assert_eq!(pgr[0].start_time, 0.0);
     let mut kfs = Vec::new();
     let mut pos = 0.;
-    
     kfs.extend(pgr[..pgr.len().saturating_sub(1)].iter().map(|it| {
         let from_pos = pos;
-        pos += (it.end_time - it.start_time) * it.value;  // 移除速度参数 r
-        Keyframe::new(it.start_time, from_pos, 2) // 不再乘以 r
+        pos += (it.end_time - it.start_time) * r * it.value;
+        Keyframe::new(it.start_time * r, from_pos, 2)
     }));
-    
     let last = pgr.last().unwrap();
-    kfs.push(Keyframe::new(last.start_time, pos, 2)); // 不再乘以 r
-    kfs.push(Keyframe::new(max_time, pos + (max_time - last.start_time) * last.value, 0)); // 不再乘以 r
-
+    kfs.push(Keyframe::new(last.start_time * r, pos, 2));
+    kfs.push(Keyframe::new(max_time, pos + (max_time - last.start_time * r) * last.value, 0));
     for kf in &mut kfs {
         kf.value /= HEIGHT_RATIO;
     }
-    
     Ok((
         AnimFloat::new(pgr.iter().map(
-            |it| Keyframe::new(it.start_time, it.value, 0) // 不再乘以 r
+            |it| Keyframe::new(it.start_time * r, it.value, 0)
         ).collect()), 
         AnimFloat::new(kfs)
     ))
