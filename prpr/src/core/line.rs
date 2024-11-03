0d3fa6fe4629b8a864f98f5167e6ea0afb43bd2e
Beks <yingxuw814@gmail.com>
2024-11-03 15:19:19 +0800

    0.1

--- prpr/src/core/line.rs
+++ prpr/src/core/line.rs
@@ -187,7 +187,7 @@ impl JudgeLine {
                 res.apply_model(|res| match &self.kind {
                     JudgeLineKind::Normal => {
                         let mut color = color.unwrap_or(res.judge_line_color);
-                        let desired_alpha: f32 = 0.2; // 设置为您想要的透明度值，例如 0.2 表示 20% 不透明度
+                        let desired_alpha: f32 = 0.1; // 设置为您想要的透明度值，例如 0.2 表示 20% 不透明度
                         color.a = desired_alpha.max(0.0).min(1.0);
                         let len = res.info.line_length;    
                         draw_line(-len, 0., len, 0., 0.01, color);
