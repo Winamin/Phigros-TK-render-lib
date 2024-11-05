8dcb7060cae867d640700ce7c0f459989c7140e2
Beks <yingxuw814@gmail.com>
2024-11-05 13:03:22 +0800

    spd

--- prpr/src/core/note.rs
+++ prpr/src/core/note.rs
@@ -211,11 +211,11 @@ impl Note {
             1.0
         }) * res.note_width;
         let ctrl_obj = &mut config.ctrl_obj;
-        let end_spd = self.end_speed * ctrl_obj.y.now_opt().unwrap_or(1.);
+        let end_spd = self.end_speed / 2.2 * ctrl_obj.y.now_opt().unwrap_or(1.);
         self.init_ctrl_obj(ctrl_obj, config.line_height);
         let mut color = self.object.now_color();
         color.a *= res.alpha * ctrl_obj.alpha.now_opt().unwrap_or(1.);
-        let spd = self.speed * ctrl_obj.y.now_opt().unwrap_or(2.2);
+        let spd = self.speed * ctrl_obj.y.now_opt().unwrap_or(1.);
 
         let line_height = config.line_height / res.aspect_ratio * spd;
         let height = self.height / res.aspect_ratio * spd;
