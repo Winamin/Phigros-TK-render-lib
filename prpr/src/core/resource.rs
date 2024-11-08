90b3de207fe594f67e72de95615014342980aa03
Beks <yingxuw814@gmail.com>
2024-11-08 23:09:48 +0800

    Update resource.rs

--- prpr/src/core/resource.rs
+++ prpr/src/core/resource.rs
@@ -468,11 +468,8 @@ impl Resource {
         let res_pack = ResourcePack::from_path(config.res_pack_path.as_ref())
             .await
             .context("Failed to load resource pack")?;
-        let vec2_ratio = if config.chart_debug{
-                vec2(1. * config.chart_ratio,-config.aspect_ratio.unwrap_or(info.aspect_ratio) * config.chart_ratio)
-            }else{
-                vec2(1., -config.aspect_ratio.unwrap_or(info.aspect_ratio))
-            };
+        
+        let vec2_ratio = vec2(1. * config.chart_ratio,-config.aspect_ratio.unwrap_or(info.aspect_ratio) * config.chart_ratio);
         let camera = Camera2D {
             target: vec2(0., 0.),
             zoom: vec2_ratio,
@@ -482,7 +479,7 @@ impl Resource {
         let mut audio = create_audio_manger(&config)?;
         let music = AudioClip::new(fs.load_file(&info.music).await?)?;
         let track_length = music.length();
-        let buffer_size = Some(512);
+        let buffer_size = Some(2048);
         let sfx_click = audio.create_sfx(res_pack.sfx_click.clone(), buffer_size)?;
         let sfx_drag = audio.create_sfx(res_pack.sfx_drag.clone(), buffer_size)?;
         let sfx_flick = audio.create_sfx(res_pack.sfx_flick.clone(), buffer_size)?;
@@ -580,11 +577,7 @@ impl Resource {
             self.camera.viewport = Some(viewport(aspect_ratio, vp));
         } else {
             self.aspect_ratio = aspect_ratio.min(vp.2 as f32 / vp.3 as f32);
-            if self.config.chart_debug{
-                self.camera.zoom.y = -self.aspect_ratio * self.config.chart_ratio;
-            }else{
-                self.camera.zoom.y = -self.aspect_ratio;
-            }
+            self.camera.zoom.y = -self.aspect_ratio * self.config.chart_ratio;
             self.camera.viewport = Some(viewport(self.aspect_ratio, vp));
         };
         true
