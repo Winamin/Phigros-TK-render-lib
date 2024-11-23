9202ce0b967fef46b078640323679645ba168bea
Beks <yingxuw814@gmail.com>
2024-11-23 16:38:55 +0800

    Update build.rs

--- prpr-pbc/build.rs
+++ prpr-pbc/build.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 
 fn main() {
-  println!("cargo:rustc-link-lib=lzma");
   println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+  println!("cargo:rustc-link-lib=lzma");
   println!("cargo:rustc-link-lib=X11");
   println!("cargo:rustc-link-lib=Xext");
   println!("cargo:rustc-link-lib=Xv");
