e149ef00c334c802a337ab46b34aa91586ae7bb6
Beks <yingxuw814@gmail.com>
2024-11-23 19:46:17 +0800

    Update build.rs

--- prpr-pbc/build.rs
+++ prpr-pbc/build.rs
@@ -2,9 +2,9 @@ extern crate cc;
 extern crate libc;
 
 fn main() {
-  println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
-  println!("cargo:rustc-link-lib=lzma");
-  println!("cargo:rustc-link-lib=X11");
-  println!("cargo:rustc-link-lib=Xext");
-  println!("cargo:rustc-link-lib=Xv");
+    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-lib=lzma");
+    println!("cargo:rustc-link-lib=X11");
+    println!("cargo:rustc-link-lib=Xext");
+    println!("cargo:rustc-link-lib=Xv");
 }
