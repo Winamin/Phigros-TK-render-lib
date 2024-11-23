74af72628892271175307a1c5aad79e6a8ca8c59
Beks <yingxuw814@gmail.com>
2024-11-23 16:01:23 +0800

    Update build.rs

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -10,4 +10,7 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-lib=static=swscale");
     println!("cargo:rustc-link-lib=static=avformat");
+    println!("cargo:rustc-link-lib=X11");
+    println!("cargo:rustc-link-lib=Xext");
+    println!("cargo:rustc-link-lib=Xv");
 }
