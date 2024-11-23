5c231f627663d1534a9d240fb7352ce2934fbbef
Beks <yingxuw814@gmail.com>
2024-11-23 20:35:51 +0800

    Delete prpr-pbc/build.rs

--- prpr-pbc/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-lib=lzma");
-    println!("cargo:rustc-link-lib=X11");
-    println!("cargo:rustc-link-lib=Xext");
-    println!("cargo:rustc-link-lib=Xv");
-}
