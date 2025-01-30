1667eaf209f21ff360510d22744dbaf3da8b2180
Link <yingxuw814@gmail.com>
2025-01-31 00:10:51 +0800

    Update game.rs

--- prpr/src/scene/game.rs
+++ prpr/src/scene/game.rs
@@ -369,7 +369,7 @@ impl GameScene {
 
         let margin = 0.046;
         let score_top = top + eps * 2.2 - (1. - p) * 0.4;
-        let ct = ui.text(&score).size(0.8).center();
+        //let ct = ui.text(&score).size(0.8).center();
         self.chart.with_element(ui, res, UIElement::Score, Some((-ct.x + 1. - margin, ct.y + score_top)), |ui, color| {
             ui.text(format!("{:07}", self.judge.score()))
                 .pos(1. - margin + 0.001, top + eps * 2.8125 - (1. - p) * 0.4)
@@ -487,8 +487,7 @@ impl GameScene {
             .pos(-1. + bar_width - 0.01, top + height / 2. - 0.03)
             .anchor(1., 0.5)
             .size(0.17867)
-            .color(Color::new(1.0, 1.0, 1.0, color.a * c.a))
-            .scale(scale)
+            .color(semi_white(0.7))
             .draw();
         Ok(())
     }
