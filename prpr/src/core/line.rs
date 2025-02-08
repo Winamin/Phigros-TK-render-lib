d3ac7166742ec7f202ace37bc1ac8fd2004edb48
Link <yingxuw814@gmail.com>
2025-02-08 18:50:22 +0800

    fix: some image show pure white before play

--- prpr/src/core/line.rs
+++ prpr/src/core/line.rs
@@ -203,6 +203,9 @@ impl JudgeLine {
                         draw_line(-len, 0., len, 0., 0.0075, color);
                     }
                     JudgeLineKind::Texture(texture, _) => {
+                        if res.time <= 0. && matches!(color, WHITE) {
+                            color = BLACK;
+                        }
                         let mut color = color.unwrap_or(WHITE);
                         color.a = alpha.max(0.0);
                         if res.config.chart_debug {
