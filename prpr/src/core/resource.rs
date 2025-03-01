8d0ff6c7c6fa74f711fc914b8f1cbb4d7ac52636
Link <yingxuw814@gmail.com>
2025-03-01 01:07:09 +0000

    Shit Mountain

--- prpr/src/core/resource.rs
+++ prpr/src/core/resource.rs
@@ -591,7 +591,7 @@ impl Resource {
     }
 
     #[inline]
-    pub fn with_model(&mut self, model: Mat3, f: impl FnOnce(&mut Self)) {
+    pub fn with_model(&mut self, model: Matrix3<f32>, f: impl FnOnce(&mut Self)) {
         let model = self.model_stack.last().unwrap() * model;
         self.model_stack.push(model);
         f(self);
