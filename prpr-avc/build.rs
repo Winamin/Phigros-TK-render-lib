52dd20a8add4a5e5d4fda1e1947f476ef1d15884
Link <yingxuw814@gmail.com>
2025-03-22 18:56:24 +0800

    更新 build.rs

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -6,7 +6,7 @@ fn main() {
         .probe("libavformat")
         .unwrap();
     println!("cargo:rustc-link-lib=bz2");
-    println!("cargo:rustc-link-lib=");
+    println!("cargo:rustc-link-lib=lzma");
     let libs_dir = std::env::var("PRPR_AVC_LIBS").unwrap_or_else(|_| format!("{}/static-lib", std::env::var("CARGO_MANIFEST_DIR").unwrap()));
     let libs_path = Path::new(&libs_dir).join(std::env::var("TARGET").unwrap());
     let libs_path = libs_path.display();
