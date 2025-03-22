8e2cf5d8824076bef2e9156b6cd9e61e26ccfa8e
Link <yingxuw814@gmail.com>
2025-03-22 19:14:23 +0800

    shit

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -5,6 +5,13 @@ fn main() {
         .atleast_version("58.76.100")
         .probe("libavformat")
         .unwrap();
+    println!("cargo:rustc-link-lib=avformat");
+    println!("cargo:rustc-link-lib=swresample");
+    println!("cargo:rustc-link-lib=avfilter");
+    println!("cargo:rustc-link-lib=avdevice");
+    println!("cargo:rustc-link-lib=avcodec");
+    println!("cargo:rustc-link-lib=avutil");
+    println!("cargo:rustc-link-lib=swscale");
     println!("cargo:rustc-link-lib=bz2");
     println!("cargo:rustc-link-lib=lzma");
     let libs_dir = std::env::var("PRPR_AVC_LIBS").unwrap_or_else(|_| format!("{}/static-lib", std::env::var("CARGO_MANIFEST_DIR").unwrap()));
