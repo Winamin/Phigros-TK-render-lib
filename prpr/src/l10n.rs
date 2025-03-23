fd8c2be34d5ffdf96b2d820f6f036ca6e3feb1c8
Link <yingxuw814@gmail.com>
2025-03-23 11:20:50 +0800

    Update l10n.rs

--- prpr/src/l10n.rs
+++ prpr/src/l10n.rs
@@ -85,9 +85,7 @@ impl L10nGlobal {
     pub fn new() -> Self {
         let mut lang_map = HashMap::new();
         let mut order = Vec::new();
-        let locale_lang: LanguageIdentifier = locale_lang.parse().unwrap_or_else(|_| {"en-US".parse().expect("Default language should be valid")
-        });
-        let locale_lang: LanguageIdentifier = locale_lang.parse().unwrap();
+        let locale_lang = get_locale().unwrap_or_else(|| String::from("en-US"));
         for (id, lang) in LANG_IDENTS.iter().enumerate() {
             lang_map.insert(lang.clone(), id);
             if *lang == locale_lang {
