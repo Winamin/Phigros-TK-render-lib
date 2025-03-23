b555101a4caca701fc1052b973b33f068748f584
Link <yingxuw814@gmail.com>
2025-03-23 11:12:00 +0800

    更新 l10n.rs

--- prpr/src/l10n.rs
+++ prpr/src/l10n.rs
@@ -85,8 +85,7 @@ impl L10nGlobal {
     pub fn new() -> Self {
         let mut lang_map = HashMap::new();
         let mut order = Vec::new();
-        let locale_lang: LanguageIdentifier = locale_lang.parse().unwrap_or_else(|_| {
-    "en-US".parse().expect("Default language should be valid")
+        let locale_lang: LanguageIdentifier = locale_lang.parse().unwrap_or_else(|_| {"en-US".parse().expect("Default language should be valid")
         });
         let locale_lang: LanguageIdentifier = locale_lang.parse().unwrap();
         for (id, lang) in LANG_IDENTS.iter().enumerate() {
