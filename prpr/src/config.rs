1e633b0b46adbd1d999dec7d94f408959f4112f1
Link <yingxuw814@gmail.com>
2025-04-03 18:38:56 +0800

    test: ui

--- prpr/src/config.rs
+++ prpr/src/config.rs
@@ -74,7 +74,7 @@ pub struct Config {
     pub volume_bgm: f32,
 
     // for compatibility
-    pub autoplay: Option<bool>,
+    pub autoplay: bool,
 }
 
 impl Default for Config {
@@ -115,7 +115,7 @@ impl Default for Config {
             volume_sfx: 1.,
             volume_bgm: 1.,
 
-            autoplay: None,
+            autoplay: false,
             show_progress_text: false,
             show_time_text: false,
         }
@@ -124,9 +124,7 @@ impl Default for Config {
 
 impl Config {
     pub fn init(&mut self) {
-        if let Some(flag) = self.autoplay {
-            self.mods.set(Mods::AUTOPLAY, flag);
-        }
+        self.mods.set(Mods::AUTOPLAY, self.autoplay);
     }
 
     #[inline]
