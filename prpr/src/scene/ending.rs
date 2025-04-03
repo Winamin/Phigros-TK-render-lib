389151626b3666a91247934db25633c8fb9efb0f
Link <yingxuw814@gmail.com>
2025-04-03 12:34:25 +0800

    Update ending.rs

--- prpr/src/scene/ending.rs
+++ prpr/src/scene/ending.rs
@@ -293,11 +293,10 @@ impl Scene for EndingScene {
             };
             let text = if self.autoplay {
                 if let Some(state) = &self.update_state {
-                    format!("NEW BEST 0000000 +{:07}", state.improvement)
+                    format!("NEW BEST 0000000 +{:07} {spd}", state.improvement)
                 } else {
-                    "NEW BEST".to_owned()
+                    format!("NEW BEST {spd}")
                 }
-                format!("{spd}")
             } else if !self.rated {
                 format!("{spd}")
             } else if let Some(state) = &self.update_state {
