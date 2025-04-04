adfecbc9e174901f9fb55f3d73835c2b5a353142
Link <yingxuw814@gmail.com>
2025-04-04 22:29:14 +0800

    modify

--- prpr/src/core/line.rs
+++ prpr/src/core/line.rs
@@ -192,18 +192,14 @@ impl JudgeLine {
             res.with_model(self.object.now_scale(), |res| {
                 res.apply_model(|res| match &self.kind {
                     JudgeLineKind::Normal => {
-                        let mut color = color.unwrap_or(res.judge_line_color);
-                        color.a *= alpha.max(0.0);
-                        if res.config.chart_debug {
-                            color.a = 0.10 + 0.90 * color.a;
-                        } else if color.a == 0.0 {
-                                return;
-                        }
                         let full_len = res.info.line_length;
-                        let exp_factor = if res.time <= 3.8 {
-                            1.0 - (1.0 - res.time / 3.8).powi(3)
+                        let duration = 4.5;
+                        let t_norm = (res.time / duration).min(1.0);
+                        let threshold = 0.5;
+                        let exp_factor = if t_norm < threshold {
+                            0.5 * (t_norm / threshold).powi(3)
                         } else {
-                            1.0
+                            0.5 + 0.5 * (1.0 - ((1.0 - t_norm) / (1.0 - threshold)).powi(3))
                         };
                         let current_len = full_len * exp_factor;
                         draw_line(-current_len, 0., current_len, 0., 0.0075, color);
