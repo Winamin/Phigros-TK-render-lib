9b53dc5a2a2055949b5144901fbd09fd7042d0b5
Link <yingxuw814@gmail.com>
2025-04-18 23:15:49 +0800

    Update build.rs

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -1,23 +1,10 @@
 use std::path::Path;
 
 fn main() {
-    pkg_config::Config::new()
-        .atleast_version("58.76.100")
-        .probe("libavformat")
-        .unwrap();
     let libs_dir = std::env::var("PRPR_AVC_LIBS").unwrap_or_else(|_| format!("{}/static-lib", std::env::var("CARGO_MANIFEST_DIR").unwrap()));
     let libs_path = Path::new(&libs_dir).join(std::env::var("TARGET").unwrap());
     let libs_path = libs_path.display();
     println!("cargo:rustc-link-search={libs_path}");
     println!("cargo:rustc-link-lib=z");
     println!("cargo:rerun-if-changed={libs_path}");
-    println!("cargo:rustc-link-lib=avformat");
-    println!("cargo:rustc-link-lib=swresample");
-    println!("cargo:rustc-link-lib=avfilter");
-    println!("cargo:rustc-link-lib=avdevice");
-    println!("cargo:rustc-link-lib=avcodec");
-    println!("cargo:rustc-link-lib=avutil");
-    println!("cargo:rustc-link-lib=swscale");
-    println!("cargo:rustc-link-lib=bz2");
-    println!("cargo:rustc-link-lib=lzma");
 }
